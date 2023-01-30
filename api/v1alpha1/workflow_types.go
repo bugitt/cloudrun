f701fe0373d9c7146b1e7b957586c6cd6de11d52
loheagn <loheagn@icloud.com>
2023-01-30 16:34:14 +0800

    Remove default value for workflow.spec.build.workingDir
    Signed-off-by: loheagn <loheagn@icloud.com>

--- api/v1alpha1/workflow_types.go
+++ api/v1alpha1/workflow_types.go
@@ -37,12 +37,11 @@ const (
 )
 
 type BuildSpec struct {
-	Context   BuilderContext `json:"context"`
-	BaseImage string         `json:"baseImage"`
-	//+kubebuilder:default:=/workspace
-	WorkingDir       *string `json:"workingDir,omitempty"`
-	Command          string  `json:"command"`
-	RegistryLocation string  `json:"registryLocation"`
+	Context          BuilderContext `json:"context"`
+	BaseImage        string         `json:"baseImage"`
+	WorkingDir       *string        `json:"workingDir,omitempty"`
+	Command          string         `json:"command"`
+	RegistryLocation string         `json:"registryLocation"`
 	//+kubebuilder:default:=push-secret
 	PushSecretName *string `json:"pushSecretName,omitempty"`
 }
